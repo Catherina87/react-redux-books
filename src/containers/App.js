533be1daedf70f180e1bc50bd4c1fa056e741e77
Catherina <catherine.pchelina@gmail.com>
2021-06-04 14:21:04 -0700

    Add sortBy to sort books by price, author

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,27 @@ import { connect } from 'react-redux';
 import { setBooks } from '../actions/books';
 //import { setFilter } from '../actions/filter';
 import App from '../components/App';
+import orderBy from 'lodash/orderBy';
 
-const mapStateToProps = (state) => ({
-  books: state.books.items,
-  isReady: state.books.isReady,
+const sortBy = (books, filterBy) => {
+  switch (filterBy) {
+    case 'all':
+      return books;
+    case 'price_high':
+      return orderBy(books, 'price', 'desc');
+    case 'price_low':
+      return orderBy(books, 'price', 'asc');
+    case 'author':
+      return orderBy(books, 'author', 'asc');
+    default:
+      return books;
+  }
+}
+
+const mapStateToProps = ({ books }) => ({
+  // books: state.books.items,
+  books: sortBy(books.items, books.filterBy),
+  isReady: books.isReady,
   //filterBy: state.books.filterBy,
 });
 
